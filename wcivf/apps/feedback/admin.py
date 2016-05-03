6cb0bad1d002dfed1cb4bc4dab19766e8d26e80e
Sym Roe <sym.roe@talusdesign.co.uk>
2016-05-03 15:13:56 +0100

    Prevent adding or deleting feedback

--- wcivf/apps/feedback/admin.py
+++ wcivf/apps/feedback/admin.py
@@ -8,4 +8,10 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('found_useful', 'comments',)
     readonly_fields = Feedback._meta.get_all_field_names()
 
+    def has_delete_permission(self, request, obj=None): # note the obj=None
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
 admin.site.register(Feedback, FeedbackAdmin)
