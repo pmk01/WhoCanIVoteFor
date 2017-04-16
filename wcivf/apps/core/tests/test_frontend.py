f2c144d6ba67ea547e20748af9d809ee822cdaa0
Sym Roe <sym.roe@talusdesign.co.uk>
2017-04-16 13:59:28 -0700

    Initial front end tests

---
+++ wcivf/apps/core/tests/test_frontend.py
@@ -0,0 +1,26 @@
+"""
+Tests for the HTML of the site.
+
+Used for making sure meta tags and important information is actually
+shown before and after template changes.
+"""
+
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+import vcr
+
+
+class TestMetaTags(TestCase):
+    important_urls = {
+        'homepage': reverse('home_view'),
+        'postcode': reverse('postcode_view', kwargs={'postcode' : 'EC1A 4EU'}),
+    }
+
+    @vcr.use_cassette(
+        'fixtures/vcr_cassettes/test_postcode_view.yaml')
+    def test_200_on_important_urls(self):
+        for name, url in self.important_urls.items():
+            req = self.client.get(url)
+            assert req.status_code == 200
