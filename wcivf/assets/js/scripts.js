996b10a3c48564846c02eeacf29b1272cbbec0ce
Sym Roe <sym.roe@talusdesign.co.uk>
2017-04-29 21:39:52 +0100

    Remove fallback from scripts

--- wcivf/assets/js/scripts.js
+++ wcivf/assets/js/scripts.js
@@ -1,12 +1,6 @@
-
-
-
-// fallback.ready(['jQuery'], function() {
-    if ($('#feedback_form input[name=found_useful]:checked').val() == undefined) {
-        $("#feedback_form .comments_container").hide()
-        $('#feedback_form input[name=found_useful]').click(function() {
-            $("#feedback_form .comments_container").show()
-        })
-    }
-
-// });
+if ($('#feedback_form input[name=found_useful]:checked').val() == undefined) {
+    $("#feedback_form .comments_container").hide()
+    $('#feedback_form input[name=found_useful]').click(function() {
+        $("#feedback_form .comments_container").show()
+    })
+}
