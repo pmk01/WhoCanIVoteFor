eb74ddda62c960cfb49b7464317c20e190bbf135
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-13 14:54:03 +0100

    Don't assumt candidates_locked exists

--- wcivf/apps/elections/managers.py
+++ wcivf/apps/elections/managers.py
@@ -67,7 +67,7 @@ class PostManager(models.Manager):
         for election_dict in post_dict['elections']:
             election = Election.objects.get(slug=election_dict['id'])
             kwargs = {}
-            if post_dict['candidates_locked']:
+            if post_dict.get('candidates_locked'):
                 if election_dict['winner_count'] == \
                         len(post_dict['memberships']):
                     kwargs['contested'] = False
