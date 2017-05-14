0b2165692e1659d6c35b1e7169f2ff8d7d1a2bf4
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-14 17:30:10 +0100

    MVP status check for AWS

--- wcivf/apps/core/urls.py
+++ wcivf/apps/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import HomePageView, PostcodeFormView
+from .views import HomePageView, PostcodeFormView, StatusCheckView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name="home_view"),
@@ -11,6 +11,9 @@ urlpatterns = [
     url(r'^about/$',
         TemplateView.as_view(template_name="about.html"),
         name="about_view"),
+    url(r'^_status_check/$',
+        StatusCheckView.as_view(),
+        name="status_check_view"),
 
     url(r'^london-mayoral-election-2016',
         PostcodeFormView.as_view(template_name='seo-landing-pages/london_mayoral_2016.html'),
--- wcivf/apps/core/views.py
+++ wcivf/apps/core/views.py
@@ -1,5 +1,9 @@
+import json
+import os
+
 from django import http
-from django.views.generic import FormView
+from django.conf import settings
+from django.views.generic import View, FormView
 from django.core.urlresolvers import reverse
 
 from .forms import PostcodeLookupForm
@@ -40,3 +44,30 @@ class PostcodeFormView(FormView):
 
 class HomePageView(PostcodeFormView):
     template_name = "home.html"
+
+
+class StatusCheckView(View):
+
+    @property
+    def server_is_dirty(self):
+        if getattr(settings, 'CHECK_HOST_DIRTY', False):
+            dirty_file_path = os.path.expanduser(
+                getattr(settings, 'DIRTY_FILE_PATH'))
+
+            if os.path.exists(dirty_file_path):
+                return True
+        return False
+
+    def get(self, request, *args, **kwargs):
+
+        status = 503
+
+        data = {
+            'ready_to_serve': False,
+        }
+
+        if not self.server_is_dirty:
+            status = 200
+            data['ready_to_serve'] = True
+
+        return http.JsonResponse(data, status=status)
