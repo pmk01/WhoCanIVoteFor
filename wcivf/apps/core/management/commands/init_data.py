7b2dc165758ee685729d8822a13274581d217b1d
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-15 18:54:43 +0100

    Look back 90 minutes

--- wcivf/apps/core/management/commands/init_data.py
+++ wcivf/apps/core/management/commands/init_data.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
             ]
         else:
             commands = [
-                ('import_people', "--recent", "--recent-minutes=30"),
+                ('import_people', "--recent", "--recent-minutes=90"),
             ]
 
 
