e578e71c7b260eb8d9f3731d7096da2c3e1f9f3d
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-15 02:29:29 +0100

    Make all needed apps use logger

--- wcivf/apps/core/db_routers.py
+++ wcivf/apps/core/db_routers.py
@@ -1,20 +1,21 @@
 class LoggerRouter(object):
 
+    apps_that_use_logger = [
+        'core',
+        'feedback',
+    ]
+
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'core':
+        if model._meta.app_label in self.apps_that_use_logger:
             return 'logger'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'core':
+        if model._meta.app_label in self.apps_that_use_logger:
             return 'logger'
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        """
-        Make sure the auth app only appears in the 'auth_db'
-        database.
-        """
-        if app_label == 'core':
+        if app_label in self.apps_that_use_logger:
             return db == 'logger'
         return None
