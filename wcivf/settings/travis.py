df686ce5dca74e80f578a9728c636e2ffbd6c8f5
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-17 09:17:29 +0100

    Don't cache things on Travis

--- wcivf/settings/travis.py
+++ wcivf/settings/travis.py
@@ -1,2 +1,8 @@
 # Use normal pipeline on travis as it doesn't work with fingerprinting
 STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+    }
+}
