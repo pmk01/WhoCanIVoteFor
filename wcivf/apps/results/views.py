5ebb84ee100de1a08e4e2f3ce169ac8f0fe26c89
Anna Powell-Smith <annapowellsmith@gmail.com>
2017-06-09 01:44:55 +0100

    Add auto-updating results table.
    Display responsively with jQuery DataTables.
    
    Shows most recent results first, by default, followed by soonest expected results.

--- wcivf/apps/results/views.py
+++ wcivf/apps/results/views.py
@@ -1,3 +1,6 @@
+import itertools
+from datetime import datetime, timedelta
+
 from django.views.generic import TemplateView
 
 from elections.models import Election
@@ -13,14 +16,33 @@ class ResultsListView(TemplateView):
         election_qs = Election.objects.filter(
             current=True).order_by('-election_weight')
         for election in election_qs:
+            results1 = ResultEvent.objects.filter(
+                post_election__election=election,
+                declaration_time__isnull=False
+            ).order_by('-declaration_time')
+            if results1:
+                print(results1[0].post_election.post.area_name)
+            results2 = ResultEvent.objects.filter(
+                post_election__election=election,
+                declaration_time__isnull=True
+            ).order_by('-expected_declaration_time')
+            results = []
+            for r in results1:
+                r.declaration_time = \
+                    r.declaration_time + timedelta(hours=1)
+                results.append(r)
+            for r in results2:
+                results.append(r)
+            for i, r in enumerate(results):
+                r.rank_order = i
+                # Hack to deal with these dates being stored wrongly!
+                # Fix after election night.
+                if r.expected_declaration_time:
+                    r.expected_declaration_time = \
+                        r.expected_declaration_time + timedelta(hours=1)
             election_dict = {
                 'election': election,
-                'results': ResultEvent.objects.filter(
-                    post_election__election=election,
-                    person_posts__elected=True,
-                ).order_by(
-                    '-declaration_time'
-                )
+                'results': results
             }
             context['elections'].append(election_dict)
         return context
