5442e18d13fc4d1e5a5f4bec108f1d13c1e1fdf5
Sym Roe <sym.roe@talusdesign.co.uk>
2018-04-16 17:56:17 +0100

    Import election metadata from EE

--- wcivf/apps/elections/management/commands/import_elections.py
+++ wcivf/apps/elections/management/commands/import_elections.py
@@ -15,9 +15,30 @@ class Command(BaseCommand):
             self.add_elections(results)
             next_page = results.get('next')
 
+        # Now import any metadata from EE
+        self.import_metadata()
+
     def add_elections(self, results):
         for election in results['results']:
             election_obj, created = Election.objects.update_or_create_from_ynr(
                 election)
             if created:
                 print("Added new election: {0}".format(election['name']))
+
+    def import_metadata(self):
+        next_page = '{}/api/elections/?current=true&metadata=1'.format(
+            settings.EE_BASE
+        )
+        while next_page:
+            req = requests.get(next_page)
+            results = req.json()
+            next_page = results.get('next')
+            for election in results['results']:
+                try:
+                    election_obj = Election.objects.get(
+                        slug=election['election_id'])
+                    election_obj.metadata = election['metadata']
+                    election_obj.save()
+                except:
+                    pass
+
