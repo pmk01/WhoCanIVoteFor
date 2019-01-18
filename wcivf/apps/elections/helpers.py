7589c2148ec7a7488cda0324c3bedd9d17e3b7b6
Sym Roe <sym.roe@talusdesign.co.uk>
2019-01-18 12:47:41 +0000

    Add ElectionIDSwitcher from YNR
    This allows a single URL to serve more than one view depending on the
    object type returned when looking up the election slug.
    
    Useful so we can have the same election ID namespace while keeping views
    distinct

--- wcivf/apps/elections/helpers.py
+++ wcivf/apps/elections/helpers.py
@@ -1,5 +1,9 @@
+from functools import update_wrapper
+
 from django.conf import settings
+
 import requests
+
 
 
 class EEHelper:
@@ -42,3 +46,36 @@ class JsonPaginator:
             yield data
 
         return
+
+
+class ElectionIDSwitcher:
+    def __init__(self, ballot_view, election_view, **initkwargs):
+        self.election_id_kwarg = initkwargs.get("election_id_kwarg", "election")
+        self.ballot_view = ballot_view
+        self.election_view = election_view
+
+    def __call__(self, request, *args, **kwargs):
+        from elections.models import PostElection
+        ballot_qs = PostElection.objects.filter(
+            ballot_paper_id=kwargs[self.election_id_kwarg]
+        )
+
+        if ballot_qs.exists():
+            # This is a ballot paper ID
+            view_cls = self.ballot_view
+        else:
+            # Assume this is an election ID, or let the election_view
+            # deal with the 404
+            view_cls = self.election_view
+
+        view = view_cls.as_view()
+
+        view.view_class = view_cls
+        # take name and docstring from class
+        update_wrapper(view, view_cls, updated=())
+        # and possible attributes set by decorators
+        # like csrf_exempt from dispatch
+        update_wrapper(view, view_cls.dispatch, assigned=())
+
+        self.__name__ = self.__qualname__ = view.__name__
+        return view(request, *args, **kwargs)
