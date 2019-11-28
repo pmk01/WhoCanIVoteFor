bc6adb48d33c6ab744cd3a22704052e21c21c212
Sym Roe <sym.roe@talusdesign.co.uk>
2019-11-28 18:40:30 +0000

    Don't assume everything is a postcode
    Fixes #429

--- wcivf/apps/core/context_processors.py
+++ wcivf/apps/core/context_processors.py
@@ -26,7 +26,7 @@ def referer_postcode(request):
     referer_parts = request.META.get("HTTP_REFERER", "")
     referer_parts = referer_parts.strip("/").split("/")
     if len(referer_parts) >= 2 and referer_parts[-2] == "elections":
-        return {
-            "referer_postcode": referer_parts[-1].upper().replace("%20", " ")
-        }
+        possible_postcode = clean_postcode(referer_parts[-1])
+        if len(possible_postcode) < 8:
+            return {"referer_postcode": possible_postcode.replace("%20", "")}
     return {}
