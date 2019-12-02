871f13d775c0e31465d714356645a874a8e8f88a
Sym Roe <sym.roe@talusdesign.co.uk>
2019-12-02 19:39:09 +0000

    Working importer + display on site

--- wcivf/apps/leaflets/management/commands/import_leaflets.py
+++ wcivf/apps/leaflets/management/commands/import_leaflets.py
@@ -28,26 +28,28 @@ class Command(BaseCommand):
                     url = None
 
     def add_leaflets(self, results):
-        for leaflets in results:
-            for leaflet in leaflets:
-                person_id = leaflet["ynr_person_id"]
-                if not person_id:
-                    continue
-                thumb_url = leaflet["first_page_thumb"]
-                leaflet_id = leaflet["pk"]
-                upload_date = datetime.strptime(
-                    leaflet["date_uploaded"].split(".")[0], "%Y-%m-%dT%H:%M:%S"
+        for leaflet in results:
+            person_id = leaflet["ynr_person_id"]
+            if not person_id:
+                continue
+            thumb_url = leaflet["first_page_thumb"]
+            leaflet_id = leaflet["pk"]
+            upload_date = datetime.strptime(
+                leaflet["date_uploaded"].split(".")[0], "%Y-%m-%dT%H:%M:%S"
+            )
+            dt_aware = tz.make_aware(upload_date, tz.get_current_timezone())
+            try:
+                person = Person.objects.get_by_pk_or_redirect_from_ynr(
+                    person_id
                 )
-                dt_aware = tz.make_aware(upload_date, tz.get_current_timezone())
-                try:
-                    person = Person.objects.get(ynr_id=person_id)
-                    leaflet_obj, created = Leaflet.objects.update_or_create(
-                        leaflet_id=leaflet_id,
-                        defaults={
-                            "thumb_url": thumb_url,
-                            "date_uploaded_to_electionleaflets": dt_aware,
-                        },
-                    )
-                    leaflet_obj.save()
-                except Person.DoesNotExist:
-                    print("No person found with id %s" % person_id)
+                leaflet_obj, created = Leaflet.objects.update_or_create(
+                    leaflet_id=leaflet_id,
+                    defaults={
+                        "thumb_url": thumb_url,
+                        "date_uploaded_to_electionleaflets": dt_aware,
+                        "person": person,
+                    },
+                )
+                leaflet_obj.save()
+            except Person.DoesNotExist:
+                print("No person found with id %s" % person_id)
