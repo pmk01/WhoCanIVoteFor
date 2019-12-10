5954da0fa2f71e49ab982135822372dd53ff589a
Sym Roe <sym.roe@talusdesign.co.uk>
2019-12-10 17:21:31 +0000

    Better leaflet display

--- wcivf/apps/leaflets/models.py
+++ wcivf/apps/leaflets/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from people.models import Person
 
 
+class LeafletQuerySet(models.QuerySet):
+    def latest_four(self):
+        return self.order_by("date_uploaded_to_electionleaflets")[:4]
+
+
 class Leaflet(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     leaflet_id = models.IntegerField()
@@ -11,3 +16,5 @@ class Leaflet(models.Model):
         null=True, blank=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = LeafletQuerySet.as_manager()
