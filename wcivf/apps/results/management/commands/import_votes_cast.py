c61f1666fabdf65e784d96abf6391bf034280d1f
Sym Roe <sym.roe@talusdesign.co.uk>
2019-12-13 04:49:35 +0000

    Add since to results import

--- wcivf/apps/results/management/commands/import_votes_cast.py
+++ wcivf/apps/results/management/commands/import_votes_cast.py
@@ -11,16 +11,17 @@ from elections.models import PostElection
 class Command(BaseCommand):
     help = "Import results from the YNR API"
 
-    # def add_arguments(self, parser):
-    # Add back in again when we have date filters in YNR
-    # parser.add_argument(
-    #     "--date", action="store", dest="date", help="The election date"
-    # )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--since", action="store", dest="since", help="The election date"
+        )
 
     def handle(self, *args, **options):
         base_url = "{}/api/next/results/?page_size=200".format(
             settings.YNR_BASE
         )
+        if options["since"]:
+            base_url = "{}&last_updated={}".format(base_url, options["since"])
         next_page = base_url
         for page in JsonPaginator(next_page, self.stdout):
             self.import_page(page["results"])
